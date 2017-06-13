df70171385997ccef4afaf044f3121fff5eea5c8
Eduard Balovnev <bedward70@mail.ru>
2017-06-13 08:48:51 +0600

    It is a adding of a save symbol method to DoFixture.

---
+++ readme.txt
@@ -0,0 +1,1 @@
+It is a adding of a save symbol method to DoFixture.
---
+++ src/main/java/ru/bedward70/fitnesse/io/B70DoFixture.java
@@ -0,0 +1,44 @@
+/*
+ MIT License https://en.wikipedia.org/wiki/MIT_License
+
+ Copyright (c) 2017, Eduard Balovnev (bedward70)
+ All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+package ru.bedward70.fitnesse.io;
+
+import fit.Fixture;
+import fitlibrary.DoFixture;
+import ru.bedward70.fitnesse.io.traverse.B70DoTraverse;
+
+/**
+ * Created by Eduard Balovnev on 11.06.17.
+ *
+ */
+public class B70DoFixture extends DoFixture {
+
+    public B70DoFixture() {
+        setTraverse(new B70DoTraverse(this));
+    }
+
+    public Object readSymbol(String symbolName) {
+        return Fixture.getSymbol(symbolName);
+    }
+}
